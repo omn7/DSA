3e0480f3cc4e625aa67afcdcd374f09c46986066
Om Narkhede <developers.omn@gmail.com>
2025-03-23 00:29:47 +0530

    day one

---
+++ datatype.java
@@ -0,0 +1,11 @@
+public class datatype {
+    public static void main(String[] args) {
+        byte a = 10;
+        short b = 20;
+        char c = 'A';
+        boolean d = true;
+        System.out.println(a + b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
---
+++ index.java
@@ -0,0 +1,18 @@
+import  java.util.*;
+public class index {
+  public static void main(String[]  args){
+    Scanner sc = new Scanner(System.in);
+    String name = sc.nextLine();
+    System.out.println("Hello " + name);
+
+    int a = 10;
+    String b = "Hello";
+    float c = 37.5f;
+    double d = 10.5;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c + d);
+    System.out.println("the value is " + a);
+
+  }
+}
