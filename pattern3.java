ee12bec016c5fab9c0ac9e625b0cfff9e563d0ca
Om Narkhede <developers.omn@gmail.com>
2025-06-10 16:18:09 +0530

    rombus

--- pattern3.java
+++ pattern3.java
@@ -4,15 +4,14 @@
 class Main {
     public static void main(String[] args) {
        
-        int n = 4;
+        int n = 6;
+        int counter = 1;
         for(int i = 1; i<=n; i++){
-            for(int j = 1; j<=n-i; j++){
-                System.out.print("");}
-            for(int j = 1; j<=i; j++){
-                System.out.print("*");
+            for(int j=1; j<=i; j++){
+                System.out.print(counter + " ");
+                counter++;
             }
-                
-            
+          
             System.out.println();
         }
     }
