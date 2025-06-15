6e3c3756ecf23a24087af7a8d3b2e730db1ee47c
Om Narkhede <developers.omn@gmail.com>
2025-06-16 00:59:50 +0530

    sum added

--- subarray.java
+++ subarray.java
@@ -1,24 +1,23 @@
-class subarray {
-    public static void sub(int numbers[]){
-        for(int i=0; i<numbers.length; i++){
-            int start = numbers[i];
-             for(int j=i; j<numbers.length; j++ ){
-            int end = numbers[j];
-                for(int k=start; k<end; k++){
-                    System.out.print( numbers[k]);
+class Subarray {
+    public static void sub(int numbers[]) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i; j < numbers.length; j++) {
+                // Print subarray from index i to j
+                for (int k = i; k <= j; k++) {
+                    System.out.print(numbers[k] + " ");
+                    
                 }
                 System.out.println();
-
+                int sum = numbers[i]+numbers[j];
+                System.out.println("sum "+sum);
+            }
+            System.out.println();
+            
         }
-        System.out.println();
-        }
-       
-
     }
-    public static void main(String[] args) {
-        int numbers[] = {2,4,5,6,7,8,9};
-        sub(numbers);
 
-        
+    public static void main(String[] args) {
+        int numbers[] = {2, 4, 5, 6, 7, 8, 9};
+        sub(numbers);
     }
 }
