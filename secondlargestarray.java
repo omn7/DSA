11ee047617182f52b499f2f315966e657089d48e
Om Narkhede <developers.omn@gmail.com>
2025-06-18 14:36:07 +0530

    secondlargestarray

---
+++ secondlargestarray.java
@@ -0,0 +1,21 @@
+class secondlargestarray {
+    public int getSecondLargest(int[] arr) {
+        if (arr.length < 2) {
+            return -1; // Not enough elements
+        }
+
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > first) {
+                second = first;
+                first = num;
+            } else if (num > second && num != first) {
+                second = num;
+            }
+        }
+
+        return (second == Integer.MIN_VALUE) ? -1 : second;
+    }
+}
