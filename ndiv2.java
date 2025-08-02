fb4930af6c32148a0df7adcd08343e1c83850c2b
Om Narkhede <developers.omn@gmail.com>
2025-08-02 20:51:01 +0530

    ndiv2.java

--- ndiv2.java
+++ ndiv2.java
@@ -7,12 +7,17 @@ public class ndiv2 {
         for(int i=0; i<arr.length; i++){
             int count = 0;
             for(int j =0; j<arr.length; j++){
-                if(arr[i] = arr[j]){
+                if(arr[i] == arr[j]){
                     count++;
                 }
+               
             }
+            if(count>n/2){
+                    System.out.println(arr[i]);
+                }
+      
         }
-       
+                
        
     }
 }
