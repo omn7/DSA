0fe516156c893676e26794f2bdc412e08a697438
Om Narkhede <developers.omn@gmail.com>
2025-08-02 20:43:32 +0530

    prefix and ndiv2

---
+++ ndiv2.java
@@ -0,0 +1,18 @@
+public class ndiv2 {
+    public static void main(String[] args) {
+        
+        int arr[]  = {1,1,1,2,3};
+        int n = arr.length;
+
+        for(int i=0; i<arr.length; i++){
+            int count = 0;
+            for(int j =0; j<arr.length; j++){
+                if(arr[i] = arr[j]){
+                    count++;
+                }
+            }
+        }
+       
+       
+    }
+}
