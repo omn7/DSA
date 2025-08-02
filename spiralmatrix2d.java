4d6898d86157a65039f0e6147f073530128505f5
Om Narkhede <developers.omn@gmail.com>
2025-08-03 00:40:39 +0530

    spiralmat

--- spiralmatrix2d.java
+++ spiralmatrix2d.java
@@ -1,31 +1,51 @@
-
-
 public class spiralmatrix2d {
     
-    // Main method
     public static void main(String[] args) {
         int matrix[][] = {
-            {1,2,3,4},
-            {5,6,7,8},
-            {9,10,11,12},
-            {13,14,15,16}
+            {1,  2,  3,  4},
+            {5,  6,  7,  8},
+            {9, 10, 11, 12},
+            {13,14, 15,16}
+        };
 
-        }
         int startRow = 0;
         int startColn = 0;
-        int endRow = matrix.length-1;
-        int endColn = matrix[0].length-1;
-        while(startRow<=endRow & startColn <= endColn ){
+        int endRow = matrix.length - 1;
+        int endColn = matrix[0].length - 1;
 
-            //column left to right
-            for(int j = startColn; j <= endColn; j++){
-                System.out.print(matrix[startRow][j]+" ");
+        while (startRow <= endRow && startColn <= endColn) {
+
+            // Column: Left to Right
+            for (int j = startColn; j <= endColn; j++) {
+                System.out.print(matrix[startRow][j] + " ");
             }
-            //top to bottom last row
-            for(int i = startRow+1; i<=endRow; i++){
-                System.out.print(matrix[i][endColn]+" ");
+
+            // Row: Top to Bottom
+            for (int i = startRow + 1; i <= endRow; i++) {
+                System.out.print(matrix[i][endColn] + " ");
             }
+
+            // Column: Right to Left (only if startRow < endRow to avoid duplicate)
+            if (startRow < endRow) {
+                for (int j = endColn - 1; j >= startColn; j--) {
+                    System.out.print(matrix[endRow][j] + " ");
+                }
+            }
+
+            // Row: Bottom to Top (only if startColn < endColn to avoid duplicate)
+            if (startColn < endColn) {
+                for (int i = endRow - 1; i > startRow; i--) {
+                    System.out.print(matrix[i][startColn] + " ");
+                }
+            }
+
+            // Move to inner layer
+            startRow++;
+            endRow--;
+            startColn++;
+            endColn--;
         }
-        
+
+        System.out.println(); // newline after output
     }
-    }
+}
