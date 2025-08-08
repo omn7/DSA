9613039ef11b2e1b830a1b05eb5e9881bc65dd01
Om Narkhede <developers.omn@gmail.com>
2025-08-09 00:27:38 +0530

    shortest dist sort

--- dsa/shortestpath.java
+++ dsa/shortestpath.java
@@ -1,8 +1,26 @@
-
-
-public class shortestpath {
+public class ShortestPath {
     public static void main(String[] args) {
-        
-   }
-   
+        int x = 0;
+        int y = 0;
+        String key = "WNEENESENNN";
+
+        for (int i = 0; i < key.length(); i++) {
+            char dir = key.charAt(i);
+
+            if (dir == 'W') {
+                x -= 1;
+            } else if (dir == 'N') {
+                y += 1;
+            } else if (dir == 'E') {
+                x += 1;
+            } else if (dir == 'S') {
+                y -= 1;
+            } else {
+                System.out.println("Key not found");
+            }
+        }
+
+        double distance = Math.sqrt((x * x) + (y * y));
+        System.out.println("Shortest distance: " + distance);
+    }
 }
