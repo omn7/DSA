a3ea54ffc93b9a3a7479e6b717769b55d6806d75
Om Narkhede <developers.omn@gmail.com>
2025-08-21 23:15:39 +0530

    overriding

---
+++ dsa/GetterAndSette.java
@@ -0,0 +1,19 @@
+public class GetterAndSette{
+    public static void main(String[] args) {
+        
+    }
+    class pen{
+        String color;
+        int tip;
+        String getColor(){
+            return this.color;
+        }
+        int getTip(){
+            return this.tip;
+        }
+       void setColor(String newColor){
+            this.color = newColor;
+        }
+        
+    }
+}
