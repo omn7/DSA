7865f7b9c2a5ad8da683478eb4c35a3ad78fdb22
Om Narkhede <developers.omn@gmail.com>
2025-10-20 00:27:27 +0530

    done

--- dsa/bruteforceprep1.java
+++ dsa/bruteforceprep1.java
@@ -4,8 +4,24 @@ public class bruteforceprep1 {
         //its calculate max of sum of sub array
 
     int arr[] = {1,2,3,4,7,8,9};
+    int maxSum = Integer.MIN_VALUE;
+        
+        for(int i=0; i<arr.length; i++){
+        maxSum = arr[i] + arr[i+1];
+        for(int j=0; j<arr.length; j++){
+            int currentSum = 0;
+            for(int k=i; k<=j; k++){
+                currentSum += arr[k];
+                if(currentSum > maxSum){
+                    maxSum = currentSum;
+                }
 
-    
+            }
+        }
+        System.out.println(maxSum);
+
+        }
+
 
     }
 }
