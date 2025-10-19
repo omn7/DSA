d4bc1fde44570cfe39353ad1092e10024b192918
Om Narkhede <developers.omn@gmail.com>
2025-10-19 23:58:22 +0530

    d0ne

---
+++ dsa/bruteforceprep1.java
@@ -0,0 +1,11 @@
+public class bruteforceprep1 {
+    public static void main(String[] args) {
+        //what does a bruteforce do
+        //its calculate max of sum of sub array
+
+    int arr[] = {1,2,3,4,7,8,9};
+
+    
+
+    }
+}
