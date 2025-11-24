6cb8cfdbe8b6d71fe2c2bb1472e1d527ac3a460b
Om Narkhede <developers.omn@gmail.com>
2025-11-24 14:28:15 +0530

    done

---
+++ dsa/HashingForAllChar.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class HashingForAllChar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = "abcdabca";
+
+        int[] hash = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            hash[s.charAt(i)]++;
+        }
+
+        System.out.print("Enter number of queries: ");
+        int q = sc.nextInt();
+        while (q-- > 0) {
+            System.out.print("Enter character to check: ");
+            char c = sc.next().charAt(0);
+            // fetch
+            System.out.println("Count: " + hash[c]);
+        }
+    }
+}
