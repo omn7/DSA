9ac92dadbc4df43b5b3e3d07ec5f5ce12ccc0f96
Om Narkhede <developers.omn@gmail.com>
2025-11-24 17:02:52 +0530

    rec

--- dsa/RecursionFabonacchi.java
+++ dsa/RecursionFabonacchi.java
@@ -1,9 +1,20 @@
 public class RecursionFabonacchi{
-    public static void fabonacchi(int n){
+    public static int fabonacchi(int n){
+        if(n<=1){
+            return n;
+        }
+        return fabonacchi(n-1)+fabonacchi(n-2);
         
 
     }
     public static void main(String[] args) {
-     
+     int count = 10; // Number of Fibonacci terms to generate
+
+        System.out.println("Fibonacci series up to " + count + " terms:");
+
+        // Loop to print the Fibonacci series up to the specified count
+        for (int i = 0; i < count; i++) {
+            System.out.print(fabonacchi(i) + " ");
+        }
     }
-}
+    }
