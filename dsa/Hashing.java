3071fda2b61d9897cb4dc16fbcbab1b9856eff7e
Om Narkhede <developers.omn@gmail.com>
2025-11-24 14:16:05 +0530

    done

--- dsa/Hashing.java
+++ dsa/Hashing.java
@@ -5,17 +5,17 @@ public class Hashing {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Read size of array
        
         int[] arr = {3,3,4,5,6,3,2};
 
-        // Read array elements
+        
         
 
         // precompute
         int[] hash = new int[13];
         for (int i = 0; i < arr.length; i++) {
             hash[arr[i]] += 1;
+            System.out.print(arr[i]);
         }
 
         // Read number of queries
