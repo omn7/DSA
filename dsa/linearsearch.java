740d126fc27cecea2eeb9b36551461b5dc661b73
Om Narkhede <developers.omn@gmail.com>
2025-12-03 15:12:44 +0530

    linear search

--- dsa/linearsearch.java
+++ dsa/linearsearch.java
@@ -1,20 +1,17 @@
-import java.util.*;
-
-public class linearSearch {
-
-    public static int number(int numbers[]) {
-        int largest = Integer.MIN_VALUE;
-
-        for (int i = 0; i < numbers.length; i++) {
-            if (largest < numbers[i]) {
-                largest = numbers[i];
-            }
-        }
-        return largest;
-    }
-
+public class linearsearch{
     public static void main(String[] args) {
-        int numbers[] = {12, 13, 14, 15};
-        System.out.print(number(numbers));
+        int[] arr = {2,3,4,5,6,7,8,9};
+        int n = arr.length;
+        int k = 3;
+        for(int i=0; i<n; i++){
+            if(arr[i]==k){
+                System.out.print("found at index  " + i);
+            }
+           
+        } 
+       
+                 System.out.print("not found");
+            
+       
     }
 }
