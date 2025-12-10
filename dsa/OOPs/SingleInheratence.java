31fd267508f5bde5a146e2ef33f30b150460b401
Om Narkhede <developers.omn@gmail.com>
2025-12-10 16:32:20 +0530

    done

---
+++ dsa/OOPs/SingleInheratence.java
@@ -0,0 +1,24 @@
+// Parent class
+class Vehicle {
+    void drive() {
+        System.out.println("Vehicle is driving");
+    }
+}
+
+// Child class extending Vehicle (Single Inheritance)
+class SingleInheratence extends Vehicle {
+    
+    SingleInheratence() {
+        System.out.println("This is my car");
+    }
+    
+   
+    void drive() {
+        System.out.println("Car is driving safely");
+    }
+    
+    public static void main(String[] args) {
+        SingleInheratence car = new SingleInheratence();
+        car.drive();
+    }
+}
