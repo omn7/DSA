bb72df3f2558bfc0048cf3b4f1cfe0602af1c676
Om Narkhede <developers.omn@gmail.com>
2026-01-11 18:56:45 +0530

    done

--- dsa/kadane.java
+++ dsa/kadane.java
@@ -1,29 +1,26 @@
-class Solution {
-    int maxSubarraySum(int[] arr) {
-        int sum = 0;
-        int max = Integer.MIN_VALUE;
-        boolean hasPositive = false;
-        for(int i=0; i<arr.length; i++){
-            
-           if(arr[i] >= 0){
-               hasPositive = true;
-           }  
-           sum = sum + arr[i];
-           max = Math.max(sum, max);
-           if(sum < 0){
-               sum = 0;
-           }
-        }
-        return hasPositive ? max : maxIntNegative(arr);
+class kadane {
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        Solution solution = new Solution();
+        int result = solution.maxSubArray(nums);
+        System.out.println("Max Subarray Sum: " + result);
     }
-    int maxIntNegative(int[] arr){
-        int max =arr[0];
-        for(int i = 1; i< arr.length; i++){
-            if(arr[i] > max){
-                max = arr[i];
+}
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0; 
+        int maxi = nums[0];
+        for(int i=0; i<nums.length; i++){
+
+            sum+= nums[i];
+            if(sum > maxi){
+                maxi = sum;
             }
-            
+            if(sum < 0){
+                sum = 0;
+            }
         }
-        return max;
+        return maxi;
     }
 }
